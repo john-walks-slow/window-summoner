f2acb8a31a5a78a69e231ebee6026c7df537b5a9
JohnnRen <john.ren.contact@gmail.com>
2023-11-22 00:22:01 +0800

    :package: Update compile process

--- compile.bat
+++ compile.bat
@@ -1,1 +1,8 @@
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "呼来唤去.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
+@REM encoding = gb2312
+@REM Set version number
+set version=0.4.0
+@REM Create version directory in release
+rm release\%version% -rf
+mkdir release\%version%
+cp data release\%version%\data -rf
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\呼来唤去.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"

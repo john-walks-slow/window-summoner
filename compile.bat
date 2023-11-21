aa5530d0195fe764c64d04e8232e043c8fc15dca
JohnnRen <john.ren.contact@gmail.com>
2023-11-22 00:54:37 +0800

    ✨ Use version number from arg

--- compile.bat
+++ compile.bat
@@ -1,10 +1,10 @@
 @REM encoding = gb2312
-@REM Set version number
-set version=0.4.0
+@REM Set version number from arg
+set version=%1
 @REM Create version directory in release
 rm release\%version% -rf
 mkdir release\%version%
 cp data release\%version%\data -rf
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\ºôÀ´»½È¥.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È¥.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
 @REM bandizip compress to zip
 "C:\Program Files\Bandizip\Bandizip.exe" c -y -r "release\%version%.zip" "release\%version%"

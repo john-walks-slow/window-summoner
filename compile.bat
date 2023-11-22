18ca21f0174c43a19235425745250bd43b3cbb54
JohnnRen <john.ren.contact@gmail.com>
2023-11-22 18:33:12 +0800

    🔧 Don't include config in release

--- compile.bat
+++ compile.bat
@@ -6,6 +6,7 @@ for /f "delims=" %%a in (data\version.txt) do set version=%%a
 rm release\%version% -rf
 mkdir release\%version%
 cp data release\%version%\data -rf
+rm release\%version%\data\config.json
 "C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\∫Ù¿¥ªΩ»•.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
 @REM bandizip compress to zip
 "C:\Program Files\Bandizip\Bandizip.exe" c -y -r "release\%version%.zip" "release\%version%"

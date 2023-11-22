9094e313208bcf12f832d7beb2eec11640e4fcff
JohnnRen <john.ren.contact@gmail.com>
2023-11-22 17:33:52 +0800

    :bug: Fix encoding

--- compile.bat
+++ compile.bat
@@ -5,6 +5,6 @@ set version=%1
 rm release\%version% -rf
 mkdir release\%version%
 cp data release\%version%\data -rf
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È¥.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\ºôÀ´»½È¥.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
 @REM bandizip compress to zip
 "C:\Program Files\Bandizip\Bandizip.exe" c -y -r "release\%version%.zip" "release\%version%"
